b5d26a83075fba18c58d9a831c9e4c4547823e0e
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-01-23 20:14:29 +0000

    finished scrape

--- scrape_pages.py
+++ scrape_pages.py
@@ -1,4 +1,5 @@
 import json
+import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -7,16 +8,19 @@ with open('urls.json','r') as urls:
     urls_dict = json.loads(content)
 
 urls = urls_dict['tutorials']
-test_url = urls[0]
+pattern = 'day-[0-9]+'
 
-page = urlopen(test_url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+for url in urls:
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    contents = soup.find("div", class_="article__main")
+    day = re.search(pattern, url)
 
-contents = soup.find("div", class_="article__main")
+    if day:
 
-with open("test.html", 'w') as test:
-    test.write(str(contents))
-
-
-
+        file_name = "./pages/" + day.group() + ".html"
+        with open(file_name, 'w') as tut:
+            tut.write(str(contents))
+    else: 
+        raise Exception("Regex Failed")
